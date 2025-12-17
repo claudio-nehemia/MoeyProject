5f1863731ba2e45f0e3243983794ee7d16172ac4
claudio-nehemia <claudio.nehemia83@gmail.com>
2025-12-18 01:12:26 +0700

    dah

--- resources/js/pages/Kontrak/Index.tsx
+++ resources/js/pages/Kontrak/Index.tsx
@@ -325,7 +325,7 @@ export default function Index({ itemPekerjaans, termins }: Props) {
                                                                                     <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                                                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12" />
                                                                                     </svg>
-                                                                                    <span>Upload TTD</span>
+                                                                                    <span>Upload KONTRAK DENGAN TTD</span>
                                                                                 </>
                                                                             )}
                                                                         </button>
